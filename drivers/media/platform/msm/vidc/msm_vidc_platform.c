ff0f165a8dae9f3f3f525f552c55cd89ebe994ac
wenchangliu <wenchangliu@google.com>
2020-02-07 15:22:22 +0800

    msm: vidc: disable decode batching feature
    This feature implemented for power optimization
    with OMX but not well compliant with Codec 2.0.
    Disable it to unblock VT frame drop issue.
    
    Bug: 149071324
    Test: VT frame rate test
    Change-Id: I187958f4da10d5936c0f0fbd5060301e55ac7f29
    Signed-off-by: Wen Chang Liu <wenchangliu@google.com>

--- drivers/media/platform/msm/vidc/msm_vidc_platform.c
+++ drivers/media/platform/msm/vidc/msm_vidc_platform.c
@@ -490,7 +490,7 @@ static struct msm_vidc_common_data sdmmagpie_common_data_v0[] = {
 /*
 	{
 		.key = "qcom,decode-batching",
-		.value = 1,
+		.value = 0,
 	},
 */
 	{
@@ -563,7 +563,7 @@ static struct msm_vidc_common_data sdmmagpie_common_data_v1[] = {
 /*
 	{
 		.key = "qcom,decode-batching",
-		.value = 1,
+		.value = 0,
 	},
 */
 	{

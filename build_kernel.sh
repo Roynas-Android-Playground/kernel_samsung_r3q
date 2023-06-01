dfb6a91143a89259e417e2aa2a477c6d4259bf25
roynatech2544 <whiteshell2544@naver.com>
2023-06-01 20:24:35 +0900

    ANDROID: drivers: Import KernelSU

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,9 +6,12 @@ set -e
 export KBUILD_BUILD_USER=Royna
 export KBUILD_BUILD_HOST=GrassLand
 
+git submodule update --init
+
 PATH=$PWD/toolchain/bin:$PATH
 COMMON_FLAGS="O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CC=clang AS=llvm-as LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump"
 rm -rf out
+
 make O=out $COMMON_FLAGS -j$(nproc) r3q_defconfig
 cp out/.config arch/arm64/configs/r3q_defconfig
 make O=out $COMMON_FLAGS -j$(nproc)

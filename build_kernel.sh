ec4be041efffefe6a62490f197ee8239e035069d
roynatech2544 <whiteshell2544@naver.com>
2023-10-19 01:01:02 +0000

    kernel: Add build script

---
+++ build_kernel.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+set -e
+
+[ ! -e "toolchain" ] && echo "Make toolchain avaliable at $(pwd)/toolchain" && exit
+
+export KBUILD_BUILD_USER=Royna
+export KBUILD_BUILD_HOST=GrassLand
+
+PATH=$PWD/toolchain/bin:$PATH
+COMMON_FLAGS="O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CC=clang AS=llvm-as LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump"
+rm -rf out
+make O=out $COMMON_FLAGS -j$(nproc) r3q_defconfig
+cp out/.config arch/arm64/configs/r3q_defconfig
+make O=out $COMMON_FLAGS -j$(nproc)

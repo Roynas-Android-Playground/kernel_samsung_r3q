6cf7e3972742679fb030e695c5ef4ffe3f94a2a1
roynatech2544 <whiteshell2544@naver.com>
2023-10-30 02:05:21 +0000

    build_kernel.sh: Switch to LLVM=1

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,10 +9,10 @@ export KBUILD_BUILD_HOST=GrassLand
 git submodule update --init
 
 PATH=$PWD/toolchain/bin:$PATH
-COMMON_FLAGS="O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CC=clang AS=llvm-as LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump"
+COMMON_FLAGS="O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc)"
 rm -rf out
 
-make O=out $COMMON_FLAGS -j$(nproc) r3q_defconfig
-make O=out $COMMON_FLAGS -j$(nproc) savedefconfig
+make $COMMON_FLAGS r3q_defconfig
+make $COMMON_FLAGS savedefconfig
 cp out/defconfig arch/arm64/configs/r3q_defconfig
-make O=out $COMMON_FLAGS -j$(nproc)
+make O=out $COMMON_FLAGS

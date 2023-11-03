0cb9d8ea72b4cf17ba04a46706f317c29e643a29
roynatech2544 <whiteshell2544@naver.com>
2023-11-03 08:12:29 +0000

    build_kernel: Add support for thinlto builds
    - And use it for actions, Github actions are slow

--- build_kernel.sh
+++ build_kernel.sh
@@ -12,7 +12,18 @@ PATH=$PWD/toolchain/bin:$PATH
 COMMON_FLAGS="O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc)"
 rm -rf out
 
+THIN=
+if [ "$1" = "thin" ]; then
+	THIN=thinlto.config
+fi
+
 make $COMMON_FLAGS r3q_defconfig
-make $COMMON_FLAGS savedefconfig
-cp out/defconfig arch/arm64/configs/r3q_defconfig
+
+echo "$0: ThinLTO: ${THIN:-"Not Enabled"}"
+if [ -z $THIN ]; then
+	make $COMMON_FLAGS savedefconfig
+	cp out/defconfig arch/arm64/configs/r3q_defconfig
+	echo "$0: Defconfig regenerated"
+fi
+
 make O=out $COMMON_FLAGS Image.gz-dtb dtbs

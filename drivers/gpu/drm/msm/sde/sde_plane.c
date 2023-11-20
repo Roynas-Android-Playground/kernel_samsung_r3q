classified/android_kernel_oneplus_sm8150

drm: msm: ignore CSC config on FOD layer
FOD performance decreases if the color of the icon is not
exactly white, which happens when night light is enabled.
Skip CSC for the FOD layer.

Change-Id: Icb202690ec58e807b612fffe479c8e7c5c3b5d03

drivers/gpu/drm/msm/sde/sde_plane.c
@@ -3963,7 +3963,9 @@ struct sde_csc_cfg *sde_plane_get_csc_cfg(struct drm_plane *plane)
 	psde = to_sde_plane(plane);
 	pstate = to_sde_plane_state(plane->state);
 
-	if (psde->csc_pcc_ptr)
+	if (sde_plane_check_fingerprint_layer(&pstate->base))
+		csc_ptr = NULL;
+	else if (psde->csc_pcc_ptr)
 		csc_ptr = psde->csc_pcc_ptr;
 	else
 		csc_ptr = psde->csc_ptr;

Roynas-Android-Playground/kernel_samsung_r3q

drivers/gpu/drm/msm/sde/sde_plane.c
@@ -3981,7 +3981,9 @@ struct sde_csc_cfg *sde_plane_get_csc_cfg(struct drm_plane *plane)
 	psde = to_sde_plane(plane);
 	pstate = to_sde_plane_state(plane->state);
 
-	if (psde->csc_pcc_ptr)
+	if (sde_plane_is_fod_layer(&pstate->base))
+		csc_ptr = NULL;
+	else if (psde->csc_pcc_ptr)
 		csc_ptr = psde->csc_pcc_ptr;
 	else
 		csc_ptr = psde->csc_ptr;
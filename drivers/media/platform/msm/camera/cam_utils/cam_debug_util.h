199081b82004ef75dd9231917c4f783dc86a08dd
roynatech2544 <whiteshell2544@naver.com>
2023-12-10 12:10:58 +0000

    media: msm: camera: Stub out log prints

--- drivers/media/platform/msm/camera/cam_utils/cam_debug_util.h
+++ drivers/media/platform/msm/camera/cam_utils/cam_debug_util.h
@@ -97,9 +97,10 @@ const char *cam_get_module_name(unsigned int module_id);
  * @fmt      :  Formatted string which needs to be print in log
  * @args     :  Arguments which needs to be print in log
  */
-#define CAM_ERR(__module, fmt, args...)                            \
+#define CAM_ERR(__module, fmt, args...)/*                            \
 	pr_err("CAM_ERR: %s: %s: %d " fmt "\n",                     \
 		cam_get_module_name(__module), __func__,  __LINE__, ##args)
+*/
 /*
  * CAM_WARN
  * @brief    :  This Macro will print warning logs
@@ -108,9 +109,11 @@ const char *cam_get_module_name(unsigned int module_id);
  * @fmt      :  Formatted string which needs to be print in log
  * @args     :  Arguments which needs to be print in log
  */
-#define CAM_WARN(__module, fmt, args...)                           \
+#define CAM_WARN(__module, fmt, args...)/*                           \
 	pr_warn("CAM_WARN: %s: %s: %d " fmt "\n",                     \
 		cam_get_module_name(__module), __func__,  __LINE__, ##args)
+*/
+
 /*
  * CAM_INFO
  * @brief    :  This Macro will print Information logs
@@ -119,9 +122,10 @@ const char *cam_get_module_name(unsigned int module_id);
  * @fmt      :  Formatted string which needs to be print in log
  * @args     :  Arguments which needs to be print in log
  */
-#define CAM_INFO(__module, fmt, args...)                           \
+#define CAM_INFO(__module, fmt, args...)/*                           \
 	pr_info("CAM_INFO: %s: %s: %d " fmt "\n",                     \
 		cam_get_module_name(__module), __func__,  __LINE__, ##args)
+*/
 
 /*
  * CAM_INFO_RATE_LIMIT
@@ -131,9 +135,10 @@ const char *cam_get_module_name(unsigned int module_id);
  * @fmt      :  Formatted string which needs to be print in log
  * @args     :  Arguments which needs to be print in log
  */
-#define CAM_INFO_RATE_LIMIT(__module, fmt, args...)                 \
+#define CAM_INFO_RATE_LIMIT(__module, fmt, args...)/*                 \
 	pr_err_ratelimited("CAM_INFO: %s: %s: %d " fmt "\n",            \
 		cam_get_module_name(__module), __func__,  __LINE__, ##args)
+*/
 
 /*
  * CAM_DBG
@@ -143,15 +148,17 @@ const char *cam_get_module_name(unsigned int module_id);
  * @fmt      :  Formatted string which needs to be print in log
  * @args     :  Arguments which needs to be print in log
  */
-#define CAM_DBG(__module, fmt, args...)                            \
+#define CAM_DBG(__module, fmt, args...)/*                            \
 	cam_debug_log(__module, __func__, __LINE__, fmt, ##args)
+*/
 
 /*
  * CAM_ERR_RATE_LIMIT
  * @brief    :  This Macro will print error print logs with ratelimit
  */
-#define CAM_ERR_RATE_LIMIT(__module, fmt, args...)                 \
+#define CAM_ERR_RATE_LIMIT(__module, fmt, args...)/*                 \
 	pr_err_ratelimited("CAM_ERR: %s: %s: %d " fmt "\n",            \
 		cam_get_module_name(__module), __func__,  __LINE__, ##args)
+*/
 
 #endif /* _CAM_DEBUG_UTIL_H_ */
